8392b620557458b267ae6b68a4be3b989b837c43
Roberto Puente <rpi@softtelecom.eu>
2019-01-16 21:01:48 +0100

    First commit

---
+++ src/main/java/com/apress/prospring4/SimpleBeanApplication.java
@@ -0,0 +1,14 @@
+package com.apress.prospring4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBeanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleBeanApplication.class, args);
+	}
+
+}
+
